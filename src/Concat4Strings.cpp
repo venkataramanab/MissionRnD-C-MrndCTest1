d3ce98584a9e5ea9879b7ff2f20f53381922a791
venkataramanab <venkataramanaedu@gmail.com>
2016-04-02 21:11:59 +0530

    Update Concat4Strings.cpp

--- src/Concat4Strings.cpp
+++ src/Concat4Strings.cpp
@@ -29,6 +29,6 @@ Difficulty : Hard
 
 char * concat_4strings(char *s1, char *s2, char *s3, char *s4){
 	//Return the new string created.
-	if (head1&&head2)
-		return -1;
+	if (s1==NULL&&s2==NULL&&s3==NULL&&s4==NULL)
+		return NULL;
 }
